6c38907f3bc1ef2b8f9b3a3b5a183bc1e0b030a8
rrjafarov <rafael.cefer.4050@gmail.com>
2025-12-06 03:51:36 +0400

    manissanda

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,76 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin({
+  locales: ["az", "en", "ru"],
+  defaultLocale: "az",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "admin.adentta.az",
+        pathname: "/**",
+      },
+    ],
+  },
+
+  // output: "export",
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "preset-default",
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
