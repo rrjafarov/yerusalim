b2411ca6bae2675fb671cdbc030e38abd49a9b11
rrjafarov <rafael.cefer.4050@gmail.com>
2026-04-14 11:50:01 +0400

    blog -date

--- components/BlogsDetailPage/BlogsDetailHero.jsx
+++ components/BlogsDetailPage/BlogsDetailHero.jsx
@@ -1,17 +1,86 @@
+// import Image from "next/image";
+// import Link from "next/link";
+// import React from "react";
+
+// const BlogsDetailHero = ({ blogData }) => {
+
+//   const formatDate = (dateString) => {
+//     const date = new Date(dateString);
+
+//     return new Intl.DateTimeFormat("en-US", {
+//       month: "short",
+//       day: "numeric",
+//       year: "numeric",
+//     }).format(date);
+//   };
+
+//   return (
+//     <div className="blogsDetailHero">
+//       <div className="container">
+//         <div className="blogsDetailHeroItem">
+//           <Image
+//             src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE}${blogData.main_image}`}
+//             alt="banner"
+//             width={1260}
+//             height={650}
+//           />
+//           <div className="blogsDetailHeroItemTitle">
+//             <span>{formatDate(blogData.published_date)}</span>
+//             <h1>{blogData.title}</h1>
+//           </div>
+//           <div className="blogsDetailHeroItemHashtag">
+//             {blogData?.hastags &&
+//               Object.values(blogData.hastags).map((hashtag, index) => (
+//                 <span key={index}>
+//                   #{hashtag.name}
+//                 </span>
+//               ))}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default BlogsDetailHero;
+
+
+
+
+
+
+
+
+
+
+
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useLocale } from "next-intl";
 
 const BlogsDetailHero = ({ blogData }) => {
+  const locale = useLocale();
+
+  const months = {
+    az: ["Yan", "Fev", "Mar", "Apr", "May", "İyn", "İyl", "Avq", "Sen", "Okt", "Noy", "Dek"],
+    en: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    ru: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+  };
 
   const formatDate = (dateString) => {
-    const date = new Date(dateString);
+    if (!dateString) return "";
 
-    return new Intl.DateTimeFormat("en-US", {
-      month: "short",
-      day: "numeric",
-      year: "numeric",
-    }).format(date);
+    const d = new Date(dateString);
+
+    if (isNaN(d.getTime())) return "";
+
+    const monthList = months[locale] || months["en"];
+    const day = d.getDate();
+    const year = d.getFullYear();
+
+    return `${monthList[d.getMonth()]} ${day}, ${year}`;
   };
 
   return (
--- components/Mixed/BlogCard.jsx
+++ components/Mixed/BlogCard.jsx
@@ -3,17 +3,33 @@
 // import React from "react";
 
 // const BlogCard = ({ title, date, image, url, id }) => {
+//   const formatDate = (dateString) => {
+//     if (!dateString) return "";
+
+//     const d = new Date(dateString);
+
+//     if (isNaN(d.getTime())) return "";
+
+//     return new Intl.DateTimeFormat("en-US", {
+//       month: "short",
+//       day: "numeric",
+//       year: "numeric",
+//     }).format(d);
+//   };
+
 //   return (
 //     <div>
 //       <div className="blogsLogo">
 //         <Link href={`/blogs/${url}-${id}`}>
 //           <Image src={image} alt="blog" width={610} height={350} />
 //         </Link>
+
 //         <div className="blogsLogoTitle">
 //           <span>{title}</span>
 //         </div>
+
 //         <div className="blogsLogoDate">
-//           <span>{date}</span>
+//           <span>{formatDate(date)}</span>
 //         </div>
 //       </div>
 //     </div>
@@ -27,11 +43,39 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useLocale } from "next-intl";
 
 const BlogCard = ({ title, date, image, url, id }) => {
+  const locale = useLocale();
+
+  const months = {
+    az: ["Yan", "Fev", "Mar", "Apr", "May", "İyn", "İyl", "Avq", "Sen", "Okt", "Noy", "Dek"],
+    en: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    ru: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+  };
+
   const formatDate = (dateString) => {
     if (!dateString) return "";
 
@@ -39,11 +83,11 @@ const BlogCard = ({ title, date, image, url, id }) => {
 
     if (isNaN(d.getTime())) return "";
 
-    return new Intl.DateTimeFormat("en-US", {
-      month: "short",
-      day: "numeric",
-      year: "numeric",
-    }).format(d);
+    const monthList = months[locale] || months["en"];
+    const day = d.getDate();
+    const year = d.getFullYear();
+
+    return `${monthList[d.getMonth()]} ${day}, ${year}`;
   };
 
   return (
@@ -66,59 +110,3 @@ const BlogCard = ({ title, date, image, url, id }) => {
 };
 
 export default BlogCard;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import Image from "next/image";
-// import Link from "next/link";
-// import React from "react";
-
-// const BlogCard = ({ title, date, image, url, id }) => {
-
-//   const formatDate = (dateString) => {
-//     const d = new Date(dateString);
-//     return new Intl.DateTimeFormat("en-US", {
-//       month: "short",
-//       day: "numeric",
-//       year: "numeric",
-//     }).format(d);
-//   };
-
-//   return (
-//     <div>
-//       <div className="blogsLogo">
-//         <Link href={`/blogs/${url}-${id}`}>
-//           <Image src={image} alt="blog" width={610} height={350} />
-//         </Link>
-//         <div className="blogsLogoTitle">
-//           <span>{title}</span>
-//         </div>
-//         <div className="blogsLogoDate">
-//           <span>{formatDate(date)}</span>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default BlogCard;
