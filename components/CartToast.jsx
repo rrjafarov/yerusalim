477ea067490c6550ba768f7ef3348afbe9b5ecb6
rrjafarov <rafael.cefer.4050@gmail.com>
2026-04-22 13:05:04 +0400

    coast cart

--- components/CartToast.jsx
+++ components/CartToast.jsx
@@ -1,9 +1,39 @@
+// "use client";
+// import { FaBasketShopping } from "react-icons/fa6";
+// import { createPortal } from "react-dom";
+// import React from "react";
+
+// const CartToast = ({ isVisible, t }) => {
+//   return createPortal(
+//     <div className={`cartToast ${isVisible ? "cartToast--visible" : ""}`}>
+//       <span>
+//         <FaBasketShopping className="toastIcon" />
+//         {t?.addedCart}
+//       </span>
+//     </div>,
+//     document.body,
+//   );
+// };
+
+// export default CartToast;
+
+// // ! alt alta
+
+
 "use client";
 import { FaBasketShopping } from "react-icons/fa6";
 import { createPortal } from "react-dom";
-import React from "react";
+import { useEffect, useState } from "react";
 
 const CartToast = ({ isVisible, t }) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return createPortal(
     <div className={`cartToast ${isVisible ? "cartToast--visible" : ""}`}>
       <span>
@@ -16,5 +46,3 @@ const CartToast = ({ isVisible, t }) => {
 };
 
 export default CartToast;
-
-// ! alt alta
