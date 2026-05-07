5409789fe1d67c004d4176ad35e0c00b4bef9f76
rrjafarov <rafael.cefer.4050@gmail.com>
2026-05-08 00:44:56 +0400

    yeni page

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
-// import { redirect } from "next/navigation";
-
-// export default function RootPage({ children }) {
-//   return <div>{children}</div>;
-// }
-
-
-
-
 import { redirect } from "next/navigation";
 
-export default function RootPage() {
-  redirect("/az");
+export default function RootPage({ children }) {
+  return <div>{children}</div>;
 }
+
+
+
+
+// import { redirect } from "next/navigation";
+
+// export default function RootPage() {
+//   redirect("/az");
+// }
